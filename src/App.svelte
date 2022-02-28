40d245c47e1d1bc91095a30cb74e23cf9897e08e
Robear Mankaryous <rmankaryous12@gmail.com>
2022-02-28 13:36:03 -0500

    s3!

--- src/App.svelte
+++ src/App.svelte
@@ -1,87 +1,152 @@
 <script lang="ts">
-  import { onMount } from "svelte";
-  import { S3Client, AbortMultipartUploadCommand } from "@aws-sdk/client-s3";
+  import { onDestroy, onMount } from "svelte";
+  import { S3Client, GetObjectCommand, S3 } from "@aws-sdk/client-s3";
 
-  import accompaniment from "./assets/accompaniment.wav";
-  import vocals from "./assets/vocals.wav";
+  const attack = 0.01;
+  const decay = 0.1;
 
-  type Song = { tag: string | URL; buffer: AudioBuffer; gain: GainNode };
+  type Song = {
+    tag: string;
+    buffer: AudioBuffer;
+    gain: GainNode;
+    gainValue: number;
+    solo: boolean;
+  };
 
   const state = {
+    keys: ["accompaniment.wav", "vocals.wav"],
     context: null as AudioContext,
     songs: [] as Array<Song>,
+    soloSong: null as string | null,
   };
 
-  const fetchSong = (url: string | URL) => {
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", url, true);
-    xhr.responseType = "arraybuffer";
-    xhr.onload = () => {
-      state.context.decodeAudioData(xhr.response, (buffer) => {
-        // create source
-        const song: Song = {
-          tag: url,
-          buffer,
-          gain: state.context.createGain(),
-        };
-        state.songs = [...state.songs, song];
-        console.log(`Retrieved ${url} buffer`);
-      });
-    };
-    xhr.send();
+  const fetchSong = async (client: S3Client, key: string) => {
+    const response = await client.send(
+      new GetObjectCommand({
+        Bucket: import.meta.env.VITE_AWS_BUCKET_STEM as string,
+        Key: key,
+      })
+    );
+
+    const stream = response.Body as ReadableStream;
+
+    const arrayBuffer = await new Response(stream).arrayBuffer();
+    state.context.decodeAudioData(arrayBuffer, (buffer) => {
+      // create source
+      const song: Song = {
+        tag: key,
+        buffer,
+        gain: state.context.createGain(),
+        gainValue: 1.0,
+        solo: false,
+      };
+      state.songs = [...state.songs, song];
+      console.log(`Retrieved ${key} buffer`);
+    });
   };
 
-  onMount(() => {
+  onMount(async () => {
     const client = new S3Client({
-      region: import.meta.env.AWS_REGION_STEM,
-      accessKeyId: import.meta.env.AWS_ACCESS_STEM,
-      secretAccessKey: import.meta.env.AWS_SECRET_STEM,
+      region: import.meta.env.VITE_AWS_REGION_STEM as string,
+      credentials: {
+        accessKeyId: import.meta.env.VITE_AWS_ACCESS_STEM as string,
+        secretAccessKey: import.meta.env.VITE_AWS_SECRET_STEM as string,
+      },
     });
 
-    console.log(client);
+    console.log(import.meta.env.VITE_AWS_REGION_STEM);
 
     state.context = new AudioContext();
 
     // convert accompaniment to buffer
-    fetchSong(accompaniment);
-    fetchSong(vocals);
+    for (const key of state.keys) {
+      fetchSong(client, key);
+    }
+  });
+
+  onDestroy(() => {
+    console.log("Destroying context");
+    state.context.close();
   });
 
   const play = (time) => {
-    state.songs.forEach((song) => {
+    Object.values(state.songs).forEach((song) => {
       const source = state.context.createBufferSource();
       source.buffer = song.buffer;
       source.connect(song.gain);
       song.gain.connect(state.context.destination);
-      source.start(time);
+      source.start(state.context.currentTime + time);
     });
     console.log("Playing tracks");
   };
 
   const setSlider = (e: Event, song: Song) => {
     const target = e.target as HTMLInputElement;
-    song.gain.gain.setTargetAtTime(
-      parseFloat(target.value),
-      state.context.currentTime,
-      0.01
-    );
+    const val = parseFloat(target.value);
+    song.gain.gain.setTargetAtTime(val, state.context.currentTime, attack);
+    song.gainValue = val;
+    state.songs = [...state.songs];
+  };
+
+  const setSolo = (soloSong: Song) => {
+    state.songs.forEach((song) => {
+      const isSong = song === soloSong;
+      song.gain.gain.setTargetAtTime(
+        isSong ? 1.0 : 0.0,
+        state.context.currentTime,
+        attack
+      );
+      song.solo = isSong;
+    });
+    state.songs = [...state.songs];
+    state.soloSong = soloSong.tag;
+  };
+
+  const resetGains = () => {
+    state.songs.forEach((song) => {
+      song.gain.gain.setTargetAtTime(
+        song.gainValue,
+        state.context.currentTime,
+        decay
+      );
+      song.solo = false;
+    });
+    state.songs = [...state.songs];
+    state.soloSong = null;
   };
 </script>
 
 <main>
   <h1>STEM!?</h1>
 
-  <button on:click={() => play(0)}>Play</button>
+  {#if Object.keys(state.songs).length === state.keys.length}
+    <button on:click={() => play(0.1)}>Play</button>
+  {/if}
+
+  {#each state.keys as key}
+    {#if !state.songs.find((song) => song.tag === key)}
+      <div>Hang tight, pulling {key} from S3...</div>
+    {/if}
+  {/each}
 
   {#each state.songs as song}
-    <p>{song.tag}</p>
-    <input
-      type="range"
-      min="0"
-      max="1"
-      step="0.01"
-      on:input={(e) => setSlider(e, song)}
-    />
+    {@const hide = state.soloSong && song.tag != state.soloSong}
+    <div class="song" style:opacity={hide ? "0.5" : "1.0"}>
+      <div>{song.tag}</div>
+      <input
+        disabled={hide}
+        type="range"
+        min="0"
+        max="1"
+        step="0.01"
+        on:input={(e) => setSlider(e, song)}
+      />
+      {#if song.solo}
+        <button on:click={() => resetGains()}>Unsolo</button>
+      {:else}
+        <button disabled={hide} on:click={() => setSolo(song)}>Solo</button>
+      {/if}
+    </div>
   {/each}
 </main>
 
@@ -98,46 +163,10 @@
     gap: 1em;
   }
 
-  .controls {
+  .song {
     display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    gap: 0.5em;
-  }
-
-  .controls button {
-    font-size: 1.5em;
-    padding: 0.5em 1em;
-    border-radius: 0.25em;
-    outline: none;
-    border: none;
-    color: #fafafa;
-    background-color: #333;
-  }
-
-  .tracks {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
+    flex-direction: row;
     align-items: center;
-    gap: 0.5em;
-  }
-
-  .track {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 0em;
-    font-size: 2em;
-  }
-
-  .track input {
-    width: min(100%, 256px);
-  }
-
-  .track progress {
-    max-height: 24px;
-    background-color: #333;
-    border-radius: 0.25em;
+    gap: 1em;
   }
 </style>
